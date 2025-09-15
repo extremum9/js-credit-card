f41ac02db5e17f4d227c2cc4378c1c94a1963893
denyschr <denprogman99@gmail.com>
2025-09-15 15:49:37 +0300

    style: format code

--- modules/utils.js
+++ modules/utils.js
@@ -4,42 +4,42 @@ export const getRandomInt = (max) => {
 
 export const getCardType = (number) => {
   const cardPatterns = {
-    'visa': /^4[0-9]{12}(?:[0-9]{3})?$/,
-    'mastercard': /^5[1-5][0-9]{14}$/,
-    'amex': /^3[47][0-9]{13}$/,
-    'discover': /^6(?:011|5[0-9]{2})[0-9]{12}$/
+    visa: /^4[0-9]{12}(?:[0-9]{3})?$/,
+    mastercard: /^5[1-5][0-9]{14}$/,
+    amex: /^3[47][0-9]{13}$/,
+    discover: /^6(?:011|5[0-9]{2})[0-9]{12}$/
   };
-  
+
   for (const [card, pattern] of Object.entries(cardPatterns)) {
     if (pattern.test(number)) {
       return card;
     }
   }
-  
+
   return 'unknown';
 };
 
 export const getCardMaskType = (number) => {
   const cardPatterns = {
-    'visa': /^4/,
-    'mastercard': /^5[1-5]/,
-    'amex': /^3[47]/,
-    'discover': /^6(011|5)/
+    visa: /^4/,
+    mastercard: /^5[1-5]/,
+    amex: /^3[47]/,
+    discover: /^6(011|5)/
   };
-  
+
   for (const [card, pattern] of Object.entries(cardPatterns)) {
     if (pattern.test(number)) {
       return card;
     }
   }
-  
+
   return 'unknown';
 };
 
 export const checkLuhn = (number) => {
   let sum = 0;
   let even = false;
-  
+
   for (let index = number.length - 1; index >= 0; index--) {
     let digit = +number[index];
     if (even && (digit *= 2) > 9) {
@@ -48,21 +48,27 @@ export const checkLuhn = (number) => {
     sum += digit;
     even = !even;
   }
-  
+
   return sum % 10 === 0;
 };
 
 export const formatCardNumber = (number) => {
   const sanitized = number.replace(/\D/g, '');
   const type = getCardMaskType(sanitized);
-  
+
   switch (type) {
     case 'amex':
-      return sanitized.slice(0, 15).replace(/(\d{4})(\d{0,6})(\d{0,5})/g, '$1 $2 $3').trim();
+      return sanitized
+        .slice(0, 15)
+        .replace(/(\d{4})(\d{0,6})(\d{0,5})/g, '$1 $2 $3')
+        .trim();
     case 'visa':
     case 'mastercard':
     case 'discover':
-      return sanitized.slice(0, 16).replace(/(\d{4})/g, '$1 ').trim();
+      return sanitized
+        .slice(0, 16)
+        .replace(/(\d{4})/g, '$1 ')
+        .trim();
     default:
       return sanitized.slice(0, 16);
   }
@@ -70,7 +76,9 @@ export const formatCardNumber = (number) => {
 
 export const formatExpiryDate = (date) => {
   const sanitized = date.replace(/\D/g, '').slice(0, 4);
-  return sanitized.length > 2 ? sanitized.replace(/(\d{2})(\d{1,2})/, '$1/$2') : sanitized;
+  return sanitized.length > 2
+    ? sanitized.replace(/(\d{2})(\d{1,2})/, '$1/$2')
+    : sanitized;
 };
 
 export const formatCVVCode = (cvv) => {
--- modules/validators.js
+++ modules/validators.js
@@ -1,4 +1,4 @@
-import {checkLuhn, getCardType} from './utils.js'
+import { checkLuhn, getCardType } from './utils.js';
 
 const EMAIL_REGEXP = /^[\w.!#$%&'*+/=?^`{|}~-]+@[a-z\d-]+\.[a-z]{2,}$/;
 
@@ -7,48 +7,48 @@ const requiredValidator = (value) => value.trim() !== '';
 const emailValidator = (email) => EMAIL_REGEXP.test(email);
 
 const cardNumberValidator = (number) => {
-	const sanitized = number.replace(/\D/g, '');
-	const type = getCardType(sanitized);
-	
-	return type === 'unknown' ? false : checkLuhn(sanitized);
+  const sanitized = number.replace(/\D/g, '');
+  const type = getCardType(sanitized);
+
+  return type === 'unknown' ? false : checkLuhn(sanitized);
 };
 
 const expiryDateValidator = (date) => {
-	const [month, year] = date.split('/').map(Number);
-	if (!month || !year || month > 12) {
-		return false;
-	}
-	
-	const currentDate = new Date();
-	const currentYear = currentDate.getFullYear() % 100;
-	const currentMonth = currentDate.getMonth() + 1;
-	
-	return year > currentYear || (year === currentYear && month >= currentMonth);
+  const [month, year] = date.split('/').map(Number);
+  if (!month || !year || month > 12) {
+    return false;
+  }
+
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear() % 100;
+  const currentMonth = currentDate.getMonth() + 1;
+
+  return year > currentYear || (year === currentYear && month >= currentMonth);
 };
 
 const CVVCodeValidator = (cvv) => /^\d{3}$/.test(cvv);
 
 const validators = {
-	required: (message) => ({
-		validate: requiredValidator,
-		message
-	}),
-	email: (message) => ({
-		validate: emailValidator,
-		message
-	}),
-	cardNumber: (message) => ({
-		validate: cardNumberValidator,
-		message
-	}),
-	expiryDate: (message) => ({
-		validate: expiryDateValidator,
-		message
-	}),
-	cvv: (message) => ({
-		validate: CVVCodeValidator,
-		message
-	})
+  required: (message) => ({
+    validate: requiredValidator,
+    message
+  }),
+  email: (message) => ({
+    validate: emailValidator,
+    message
+  }),
+  cardNumber: (message) => ({
+    validate: cardNumberValidator,
+    message
+  }),
+  expiryDate: (message) => ({
+    validate: expiryDateValidator,
+    message
+  }),
+  cvv: (message) => ({
+    validate: CVVCodeValidator,
+    message
+  })
 };
 
 export default validators;
