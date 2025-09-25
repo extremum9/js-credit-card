2d3f920752d3a6036b4bed9061564a919cef04a8
denyschr <denprogman99@gmail.com>
2025-09-25 23:05:44 +0300

    feat: animate card number

--- modules/card-form.js
+++ modules/card-form.js
@@ -18,6 +18,7 @@ const CARD_MASKS = {
 };
 
 const HIGHLIGHT_ANIMATION_MS = 300;
+const SLIDE_FADE_UP_ANIMATION_MS = 300;
 
 class CardForm {
   selectors = {
@@ -34,7 +35,13 @@ class CardForm {
   stateClasses = {
     flip: 'flip',
     highlight: 'highlight',
-    hasError: 'has-error'
+    hasError: 'has-error',
+    slideFadeUpEnterActive: 'slide-fade-up-enter-active',
+    slideFadeUpLeaveActive: 'slide-fade-up-leave-active',
+    slideFadeUpEnterFrom: 'slide-fade-up-enter-from',
+    slideFadeUpLeaveFrom: 'slide-fade-up-leave-from',
+    slideFadeUpEnterTo: 'slide-fade-up-enter-to',
+    slideFadeUpLeaveTo: 'slide-fade-up-leave-to'
   };
 
   stateAttributes = {
@@ -82,6 +89,7 @@ class CardForm {
       cardHolder: this.cardHolderOutput,
       cardExpiryDate: this.cardExpiryDateOutput
     };
+    this.cardNumberItems = this.cardNumberOutput.querySelectorAll('div');
     this.fieldFocused = false;
     this.highlightTimeout = null;
     this.bindEvents();
@@ -146,12 +154,59 @@ class CardForm {
     this.cardHighlighter.classList.remove(this.stateClasses.highlight);
   }
 
+  animateCardNumber(cardNumber) {
+    const {
+      slideFadeUpEnterActive,
+      slideFadeUpLeaveActive,
+      slideFadeUpEnterFrom,
+      slideFadeUpLeaveFrom,
+      slideFadeUpEnterTo,
+      slideFadeUpLeaveTo
+    } = this.stateClasses;
+
+    this.cardNumberItems.forEach((item, index) => {
+      const currentChild = item.children[0];
+      const currentChar = currentChild.textContent.trim();
+      const newChar = cardNumber[index]?.trim() ?? '#';
+
+      if (
+        !currentChar ||
+        currentChar === newChar ||
+        currentChild.classList.contains(slideFadeUpLeaveActive)
+      ) {
+        return;
+      }
+
+      const newChild = document.createElement('span');
+      newChild.className = `${slideFadeUpEnterActive} ${slideFadeUpEnterFrom}`;
+      newChild.textContent = newChar;
+
+      currentChild.classList.add(slideFadeUpLeaveActive, slideFadeUpLeaveFrom);
+      currentChild.after(newChild);
+
+      requestAnimationFrame(() => {
+        currentChild.classList.remove(slideFadeUpLeaveFrom);
+        currentChild.classList.add(slideFadeUpLeaveTo);
+
+        newChild.classList.remove(slideFadeUpEnterFrom);
+        newChild.classList.add(slideFadeUpEnterTo);
+
+        setTimeout(() => {
+          currentChild.remove();
+          newChild.className = '';
+        }, SLIDE_FADE_UP_ANIMATION_MS);
+      });
+    });
+  }
+
   onCardNumberInput = (event) => {
     const target = event.target;
-    const formatted = formatCardNumber(target.value);
-    target.value = formatted;
+    const cardNumber = formatCardNumber(target.value);
+    target.value = cardNumber;
 
-    this.updateCardType(getCardType(formatted));
+    this.animateCardNumber(cardNumber);
+
+    this.updateCardType(getCardType(cardNumber));
     this.tabForward(target);
   };
 
@@ -197,6 +252,8 @@ class CardForm {
     const highlightElement = this.cardHighlightElements[target.id];
     if (highlightElement) {
       this.highlight(highlightElement);
+    } else {
+      this.clearHighlight();
     }
 
     if (target === this.cardCVVCodeControl) {
