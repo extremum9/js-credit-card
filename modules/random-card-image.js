c11ec40263f2811d80228280d15e350263aeb9fe
extremum9 <denprogman99@gmail.com>
2025-10-22 19:39:59 +0300

    style: format random-card-image module

--- modules/random-card-image.js
+++ modules/random-card-image.js
@@ -1,11 +1,13 @@
-import {getRandomInt} from './utils.js'
+import { getRandomInt } from './utils.js';
 
 const TOTAL_IMAGES = 14;
 
 const generateRandomCardImage = () => {
-	const randomImage = `url(../images/wallpaper-${getRandomInt(TOTAL_IMAGES)}.jpeg)`;
-	const card = document.querySelector('.js-card');
-	card.style.setProperty('--card-background-image', randomImage);
+  const randomImage = `url(../images/wallpaper-${getRandomInt(
+    TOTAL_IMAGES
+  )}.jpeg)`;
+  const card = document.querySelector('.js-card');
+  card.style.setProperty('--card-background-image', randomImage);
 };
 
 export default generateRandomCardImage;
